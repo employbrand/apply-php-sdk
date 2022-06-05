6b08ce7ef0432aa1b0b78f5392191586620022e2
Bart Fijneman <info@bartfijneman.com>
2022-06-05 18:35:33 +0200

    API fix

--- src/Entity/Environment.php
+++ src/Entity/Environment.php
@@ -16,8 +16,22 @@ class Environment extends AbstractEntity
 
     public ?string $version = null;
 
+    public ?int $usersCount = null;
+
+    public ?int $invitesCount = null;
+
+    public ?EnvironmentType $environmentType = null;
+
     public ?string $updatedAt = null;
 
     public ?string $createdAt = null;
+
+
+    public function build(array $parameters): void
+    {
+        parent::build($parameters);
+
+        $this->environmentType = new EnvironmentType($parameters[ 'environment_types' ]);
+    }
 
 }
